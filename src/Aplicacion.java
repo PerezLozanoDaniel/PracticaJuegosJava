3f9b1931b76c00d693177f792e93b9b85bcd7b7f
PerezLozanoDaniel <perez.lozano.daniel@gmail.com>
2023-05-11 12:27:12 +0200

    Punto 3.1 Ejercicio 2 Completado

--- src/Aplicacion.java
+++ src/Aplicacion.java
@@ -1,6 +1,12 @@
 public class Aplicacion {
     public static void main(String[] args) {
-        JuegoAdivinaNumero nuevoJuego =new JuegoAdivinaNumero(5,7);
+        JuegoAdivinaNumero nuevoJuego =new JuegoAdivinaNumero(3,7);
+        JuegoAdivinaPar nuevoJuegoPar =new JuegoAdivinaPar(3,4);
+        JuegoAdivinaImpar nuevoJuegoImpar =new JuegoAdivinaImpar(3,9);
+
+
         nuevoJuego.Juega();
+        nuevoJuegoPar.Juega();
+        nuevoJuegoImpar.Juega();
     }
 }
